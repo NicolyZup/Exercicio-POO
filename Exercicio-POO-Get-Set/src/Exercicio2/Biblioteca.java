906b9d3e8c07530f52ae8962f22f9d6a5cc965c1
NicolyZup <nicoly.vitorio@zup.com.br>
2023-06-20 16:21:31 -0300

    Feito exercicio 2

--- Exercicio-POO-Get-Set/src/Exercicio2/Biblioteca.java
+++ Exercicio-POO-Get-Set/src/Exercicio2/Biblioteca.java
@@ -1,4 +1,63 @@
 package Exercicio2;
 
-public abstract class Biblioteca {
+public class Biblioteca {
+    private String genero;
+    private String nomeLivro;
+    private int registro;
+    private String autor;
+
+  /*  public Biblioteca(String genero, String nomeLivro, int registro, String autor) {
+        this.genero = genero;
+        this.nomeLivro = nomeLivro;
+        this.registro = registro;
+        this.autor = autor;
+    }*/
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getNomeLivro() {
+        return nomeLivro;
+    }
+
+    public void setNomeLivro(String nomeLivro) {
+        this.nomeLivro = nomeLivro;
+    }
+
+    public int getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(int registro) {
+        this.registro = registro;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void registrarLivro(int registro){
+        System.out.println("O livro foi registrado com o isbn: "+registro);
+    }
+
+    public void registrarLivro(int registro, String nome ){
+        System.out.println("O livro foi registrado com o isbn: "+registro+" e o nome: "+nome);
+    }
+
+    public void registrarLivro(int registro, String nome, String genero){
+        System.out.println("O livro foi registrado com o isbn: "+registro+" e o nome: "+nome+" e o gênero: "+genero);
+    }
+
+    public void registrarLivro(int registro, String nome, String genero,String autor){
+        System.out.println("O livro foi registrado com todos os atributos");
+    }
 }
--- Exercicio-POO-Get-Set/src/Exercicio2/RegistrarLivro.java
+++ Exercicio-POO-Get-Set/src/Exercicio2/RegistrarLivro.java
@@ -1,2 +1,15 @@
-package Exercicio2;public class RegistrarLivro {
+package Exercicio2;
+
+public class RegistrarLivro {
+    public static void main(String[] args) {
+        Biblioteca livro = new Biblioteca();
+
+        livro.registrarLivro(2222);
+
+        livro.registrarLivro(3333, "Harry Potter");
+
+        livro.registrarLivro(4444,"Pequeno Principe","Drama");
+
+        livro.registrarLivro(5555,"Crepúsculo","Ficção","Não lembro");
+    }
 }
